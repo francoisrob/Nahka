7b51215cd03796e1a0070fd576357f538a1404d8
Francois Robbertze <francoisdprob@gmail.com>
2023-05-29 19:57:12 +0200

    feat(cdn):added cdn

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 <head>
     <meta charset="utf-8">
     <title>Nahka - Leather</title>
+    <link rel="icon" href="https://nahka.fra1.cdn.digitaloceanspaces.com/favicon.svg" type="image/svg+xml">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description"
@@ -11,14 +12,14 @@
     <meta property="og:title" content="Nahka - Leather">
     <meta property="og:description"
         content="Discover a world of exquisite craftsmanship and timeless style at Nahka. We offer a wide range of premium leather products, handcrafted with precision and passion. From luxurious bags and wallets to stylish accessories, our collection is curated to elevate your everyday style. Experience the unmatched elegance of genuine leather that combines durability with unmatched sophistication. Shop now and indulge in the art of leather craftsmanship.">
-    <meta property="og:image" content="https://nahka.studio/Assets/ogimage.png">
+    <meta property="og:image" content="https://nahka.fra1.cdn.digitaloceanspaces.com/ogimage.png">
     <meta name="twitter:card" content="summary_large_image">
     <meta property="twitter:domain" content="nahka.studio">
     <meta property="twitter:url" content="https://nahka.studio/">
     <meta name="twitter:title" content="Nahka - Leather">
     <meta name="twitter:description"
         content="Discover a world of exquisite craftsmanship and timeless style at Nahka. We offer a wide range of premium leather products, handcrafted with precision and passion. From luxurious bags and wallets to stylish accessories, our collection is curated to elevate your everyday style. Experience the unmatched elegance of genuine leather that combines durability with unmatched sophistication. Shop now and indulge in the art of leather craftsmanship.">
-    <meta name="twitter:image" content="https://nahka.studio/Assets/ogimage.png">
+    <meta name="twitter:image" content="https://nahka.fra1.cdn.digitaloceanspaces.com/ogimage.png">
     <meta name="author" content="Francois Robbertze">
     <link rel="stylesheet" href="/css/style.css">
     <script>0</script>
