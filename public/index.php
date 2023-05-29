feb60bf9b4d14794952aea136a969637570b89d6
Francois Robbertze <francoisdprob@gmail.com>
2023-05-29 18:19:43 +0200

    fix(https): removed protocol from links

--- public/index.php
+++ public/index.php
@@ -2,22 +2,23 @@
 require_once __DIR__ . '/../src/config/config.php';
 ?>
 <!DOCTYPE html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Nahka Handmade Leather">
     <meta name="author" content="Francois Robbertze">
-    <link rel="icon" href="<?php echo URL_ROOT ?>/Assets/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="/Assets/favicon.svg" type="image/svg+xml">
     <title>Nahka</title>
-    <link rel="stylesheet" href="<?php echo URL_ROOT ?>/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <?php
 session_start();
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-require_once __DIR__."/../src/routes/web.php";
+require_once __DIR__ . "/../src/routes/web.php";
 
 include_once "../src/Views/Partials/navbar.php";
 
