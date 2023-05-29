bf5d0326acf6cb4ef13db3fabbf08c3f77cdf15b
Francois Robbertze <francoisdprob@gmail.com>
2023-05-29 18:27:27 +0200

    fix(web):fix routing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Nahka
+
+Nahka is a simple e-commerce website built with PHP and MySQL.
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,11 +5,11 @@ use Symfony\Component\Routing\RouteCollection;
 
 // Routes system
 $routes = new RouteCollection();
-$routes->add('homepage', new Route(URL_SUBFOLDER . '/', array('controller' => 'PageController', 'method' => 'indexAction'), array()));
-$routes->add('product', new Route(URL_SUBFOLDER . '/product/{id}', array('controller' => 'PageController', 'method' => 'productAction'), array('id' => '[0-9]+')));
-$routes->add('cart', new Route(URL_SUBFOLDER . '/cart', array('controller' => 'PageController', 'method' => 'cartAction'), array()));
+$routes->add('homepage', new Route('/', array('controller' => 'PageController', 'method' => 'indexAction'), array()));
+$routes->add('product', new Route('/product/{id}', array('controller' => 'PageController', 'method' => 'productAction'), array('id' => '[0-9]+')));
+$routes->add('cart', new Route('/cart', array('controller' => 'PageController', 'method' => 'cartAction'), array()));
 
 //Auth routes
-$routes->add('login', new Route(URL_SUBFOLDER . '/login', array('controller' => 'PageController', 'method' => 'loginAction', ), array()));
-$routes->add('register', new Route(URL_SUBFOLDER . '/register', array('controller' => 'PageController', 'method' => 'registerAction'), array()));
-$routes->add('logout', new Route(URL_SUBFOLDER . '/logout', array('controller' => 'PageController', 'method' => 'logoutAction'), array()));
+$routes->add('login', new Route('/login', array('controller' => 'PageController', 'method' => 'loginAction', ), array()));
+$routes->add('register', new Route('/register', array('controller' => 'PageController', 'method' => 'registerAction'), array()));
+$routes->add('logout', new Route('/logout', array('controller' => 'PageController', 'method' => 'logoutAction'), array()));
